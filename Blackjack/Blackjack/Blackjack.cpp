54476c63f04091f754c8f7705d2791b6742546e2
AdrianTong99 <162846609+AdrianTong99@users.noreply.github.com>
2026-03-09 08:37:49 -0400

    Add files via upload

---
+++ Blackjack/Blackjack/Blackjack.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int input = 0;
+    bool exit = false;
+
+    //Start menu
+    cout << "Welcome to BlackJack!\n\nPlease pick one of the following options" << endl;
+
+    while (!exit) {
+        cout << "1. Create a new profile\n2.Load a profile\n3.exit" << endl;
+        cout << "Select option 1-3" << endl;
+
+        switch (input) {
+
+        }
+    }
+    
+
+    //Deal
+    //Play hand
+    //Dealer simulation
+    //Check win condition
+    //Payout
+
+    printf("\033[H\033[J");
+}
